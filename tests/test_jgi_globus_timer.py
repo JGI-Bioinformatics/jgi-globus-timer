b41c70431cbcf327725f09311ffadfe3484d48fb
Mario Melara <maamelara@gmail.com>
2022-06-13 16:42:34 -0700

    Skeleton work for timer script

--- tests/test_jgi_globus_timer.py
+++ tests/test_jgi_globus_timer.py
@@ -1,5 +1,12 @@
-from jgi_globus_timer import __version__
+from jgi_globus_timer import __version__, main
+import pytest
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_error_if_no_credential_file_exists():
+    no_inifile = "/path/to/.inifile"
+    with pytest.raises(FileNotFoundError):
+        config = main.read_secrets_ini(no_inifile)
